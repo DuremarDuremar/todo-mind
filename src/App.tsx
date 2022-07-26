7108dd10ce6dbaf340b14fe9eb7b5411cf74ee24
Ivan <duremar98@yandex.ru>
2022-07-27 01:04:42 +0300

    style

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,22 @@
 import React, { FC } from "react";
 
+import Form from "./components/form";
+import List from "./components/list";
+import { Global, Wrapper, Content } from "./styles/app_style";
+
 const App: FC = () => {
-  return <div>11</div>;
+  return (
+    <>
+      <Global />
+      <Wrapper>
+        <Content>
+          <h1>todos</h1>
+          <Form />
+          <List />
+        </Content>
+      </Wrapper>
+    </>
+  );
 };
 
 export default App;
---
+++ src/styles/app_style.tsx
@@ -0,0 +1,61 @@
+import styled, { createGlobalStyle } from "styled-components";
+
+export const Global = createGlobalStyle`
+* {
+  padding: 0;
+  margin: 0;
+  border: 0;
+  user-select: none;
+}
+body {
+  margin: 0;
+  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen",
+    "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue",
+    sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  ::-webkit-scrollbar {
+    width: 10px;
+    background-color: #f9f9fd;
+}
+::-webkit-scrollbar-thumb {
+    background-color: #0F2027;
+}
+}
+code {
+  font-family: source-code-pro, Menlo, Monaco, Consolas, "Courier New",
+    monospace;
+}
+a {
+  text-decoration: none;
+}
+ul li {
+  list-style: none;
+}
+button {
+  outline: none;
+  cursor: pointer;
+}
+input{
+  outline: none;
+}
+
+`;
+
+export const Wrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+`;
+
+export const Content = styled.div`
+  background-color: #f5f5f5;
+  font-family: "Be Vietnam Pro", sans-serif;
+
+  h1 {
+    color: #e9d9d8;
+    font-weight: 300;
+    font-size: 170px;
+  }
+`;
