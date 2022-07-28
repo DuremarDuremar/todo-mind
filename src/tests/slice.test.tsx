cef8b29f8fa26e0c440ad9b0d00ddbb8db4f10ef
Ivan <duremar98@yandex.ru>
2022-07-28 16:13:04 +0300

    tests

--- src/tests/slice.test.tsx
+++ src/tests/slice.test.tsx
@@ -17,18 +17,67 @@ describe("Main", () => {
 
   it("should add new todo item with addTodo action", () => {
     const action = { type: addTodo.type, payload: "new" };
-
     const res = todoReducer(
       {
         todos: [],
       },
       action
     );
-
     expect(res.todos[0].text).toBe("new");
+    expect(res.todos[0].completed).toBe(false);
+  });
+
+  it("should toggle todo completed status with toggleTodo action", () => {
+    const todos = [
+      {
+        id: "123",
+        text: "action.payload",
+        completed: false,
+      },
+    ];
+    const action = { type: toggleTodo.type, payload: "123" };
+
+    const res = todoReducer(
+      {
+        todos: todos,
+      },
+      action
+    );
+
+    expect(res.todos[0].completed).toBe(true);
+  });
+
+  it("should clear todo by status true with toggleTodo action", () => {
+    const todos = [
+      {
+        id: "123",
+        text: "action.payload",
+        completed: false,
+      },
+      {
+        id: "456",
+        text: "action.payload",
+        completed: true,
+      },
+      {
+        id: "789",
+        text: "action.payload",
+        completed: true,
+      },
+    ];
+
+    const action = { type: clearTodo.type };
+
+    const res = todoReducer(
+      {
+        todos: todos,
+      },
+      action
+    );
+
+    expect(res.todos.length).toEqual(1);
   });
 });
-
 // test('should return the initial state', () => {
 //   expect(todoReducer(undefined, { type: undefined })).toEqual([
 //     { text: 'Use Redux', completed: false, id: "0" }
